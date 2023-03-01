39b82754e9d59bfbcbb55b0012db0765840fded6
root <cydios76@gmail.com>
2023-03-01 16:35:16 +0800

    update

--- scriptforxui.sh
+++ scriptforxui.sh
@@ -90,6 +90,6 @@ cp -r ./scriptforxui  /usr/local/x-ui/plugs
 # wget -N --no-check-certificate -O  /usr/local/x-ui/plugs/showdatausage.sh https://github.com/shieldfree/Scriptforxui/raw/main/showdatausage.sh
 # wget -N --no-check-certificate -O  /usr/local/x-ui/plugs/xuiplug_show_usage.py https://github.com/shieldfree/Scriptforxui/raw/main/xuiplug_show_usage.py
 # wget -N --no-check-certificate -O  /usr/local/x-ui/plugs/xuiplug_show_usage_uninstall.py https://github.com/shieldfree/Scriptforxui/raw/main/xuiplug_show_usage_uninstall.py
-cd 
+
 bash /usr/local/x-ui/plugs/installplugs.sh
 
