0508fde76f2be62955fa56122e7cb50b73e0849d
root <cydios76@gmail.com>
2023-03-01 16:58:35 +0800

    upload

--- installplugs.sh
+++ installplugs.sh
@@ -47,6 +47,46 @@ before_show_menu() {
     show_menu
 }
 
+enable_port_changer() {
+    clear
+    LOGI "  正在安装 portchanger 插件..."
+    # mkdir -p /usr/local/x-ui/plugs/
+    # cp ./port_changer.py  /usr/local/x-ui/plugs/xuiplug_show_usage.py
+    # cp ./xuiplug_show_usage_uninstall.py  /usr/local/x-ui/plugs/xuiplug_show_usage_uninstall.py
+    # python3 /usr/local/x-ui/plugs/xuiplug_show_usage.py 
+
+    crontab -l | grep -v "port_changer" | crontab -
+
+    crontab -l >/tmp/crontabTask.tmp
+    echo "* 2 * * * python3 /usr/local/x-ui/plugs/port_changer.py " >>/tmp/crontabTask.tmp
+    crontab /tmp/crontabTask.tmp
+    rm /tmp/crontabTask.tmp
+
+    sleep 1
+    LOGI "  成功安装 port_changer 插件 !!"
+    LOGI "  Successfully installed !"
+}
+
+disable_port_changer() {
+    clear
+    LOGI "  正在卸载 port_changer 插件..."
+    LOGI "  Unnstalling..."
+    sleep 2
+
+    # if [[ -f /etc/x-ui/x-ui.db ]]; then
+    #     python3 /usr/local/x-ui/plugs/xuiplug_show_usage_uninstall.py
+    # fi
+    #rm  -rf /usr/local/x-ui/plugs/xuiplug_show_usage*
+
+    crontab -l | grep -v "port_changer" | crontab -
+    if [[ $? -ne 0 ]]; then
+        LOGI "  卸载 port_changer 插件失败.."
+    else
+        LOGI "  卸载数据流量插件成功 !!"
+        LOGI "  Successfully removed !!"
+    fi
+}
+
 enable_data_usage() {
     clear
     LOGI "  正在安装插件(Installing)..."
@@ -117,8 +157,12 @@ show_menu() {
   
     ${green}0.${plain} ${red}退出脚本 (Exit)${plain} 
  ——————————————————————————
-    ${green}1.${plain} 安装插件(Install)
-    ${green}2.${plain} 卸载插件(Uninstall)
+    ${green}1.${plain} 搭建订阅服务器
+    ${green}2.${plain} 删除订阅服务器
+    ${green}3.${plain} 安装端口++插件
+    ${green}4.${plain} 卸载端口++插件
+    ${green}5.${plain} remark显示用量
+    ${green}6.${plain} remark删除用量
  ——————————————————————————
  "
     echo "  Please input a number [0-2]  "
@@ -129,11 +173,31 @@ show_menu() {
         exit 0
         ;;
     1)
-        check_install && enable_data_usage
+        check_install && echo "comming soon.."
         sleep 1
         show_menu 
         ;;
     2)
+        check_install && echo "comming soon.."
+        sleep 1
+        show_menu 
+        ;;
+    3)
+        check_install && enable_port_changer
+        sleep 1
+        show_menu 
+        ;;
+    4)
+        check_install && disable_port_changer
+        sleep 1
+        show_menu 
+        ;;
+    5)
+        check_install && enable_data_usage
+        sleep 1
+        show_menu 
+        ;;
+    6)
         disable_data_usage
         sleep 2
         show_menu 
