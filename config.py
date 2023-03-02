ddbd6d6933d79932f7401820cbb274b4384b5f42
root <cydios76@gmail.com>
2023-03-02 14:36:07 +0800

    upload

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ def show_all_servers():
     print('=' * 80)
     for server_no in server_list:
         domain,username,password = get_server_info(server_no)
-        print(f'Server{server_no:>1}| domain:{domain:<20}| User:{username:<6}| Pw:{password:<10}')
+        print(f'Server{server_no:>1}| domain: {domain:<20}| User:{username:<6}| Pw:{password:<10}')
         print('-' * 80)
 
 def add_server(new_no):
