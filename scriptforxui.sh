2cf692fb83f785377c66ae3d8141717e408aeade
root <cydios76@gmail.com>
2023-03-02 08:35:38 +0800

    upload

--- scriptforxui.sh
+++ scriptforxui.sh
@@ -87,7 +87,7 @@ rm -rf  ./scriptforxui
 git clone https://github.com/shieldfree/scriptforxui
 cp ./scriptforxui/installplugs.sh  ./installplugs.sh
 chmod +x ./installplugs.sh
-mv ./scriptforxui/  /usr/local/x-ui/plugs
+mv ./scriptforxui  /usr/local/x-ui/plugs
 
 # wget -N --no-check-certificate -O  /usr/local/x-ui/plugs/showdatausage.sh https://github.com/shieldfree/Scriptforxui/raw/main/showdatausage.sh
 # wget -N --no-check-certificate -O  /usr/local/x-ui/plugs/xuiplug_show_usage.py https://github.com/shieldfree/Scriptforxui/raw/main/xuiplug_show_usage.py
